675601347d8263c42cfc1323e84f7013a7ab402e
Mohammad Gholami <gholamipersonal@gmail.com>
2019-08-20 15:48:05 +0100

    Added the wishes contoller 2

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,7 +1,7 @@
 class WishesController < ApplicationController
   def create
     @wish = Wish.new(plant_id: params[:plant_id], user_id: current_user)
-    @wish.user = current_user
+    @wish.user = current_use
     @wish.save
     authorize @wish
   end
