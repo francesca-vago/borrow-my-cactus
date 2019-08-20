d156718b487d1a2ab102641879f9ffe4d117efde
Paul Lecomte <paullecomte07@gmail.com>
2019-08-20 15:26:53 +0100

    dashborad with all the sections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
   def dashboard
-    @my_own_plants = Plant.where(user_id: current_user.id)
-    @my_booking_plant_id = Booking.where(user_id: current_user.id)
 
-    @plant_borrowed = Plant.where(id: @my_booking_plant_id)
+    @my_own_plants = Plant.where(user_id: current_user.id)
+    @my_bookings = Booking.where(user_id: current_user.id)
 
   end
 end
