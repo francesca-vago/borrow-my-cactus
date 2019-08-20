9c7dad478fe57b4dbc7dec8b8d86854bbb561a78
Francesca Vago <vago.francesca@gmail.com>
2019-08-20 12:15:15 +0100

    change routes to nest bookings

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,10 @@ Rails.application.routes.draw do
   get 'dashboard', to: 'users#dashboard', as: :dashboard
 
 
- resources :plants
+ resources :plants do
+  resources :bookings
+ end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :bookings
-
 end
