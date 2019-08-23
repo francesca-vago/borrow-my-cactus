98ee4a09f2b12103171273404338b9f3f17599f3
Francesca Vago <vago.francesca@gmail.com>
2019-08-23 14:02:44 +0100

    add style to booking cards

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -11,11 +11,9 @@ const initMapbox = () => {
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
 
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup) // add this
       .addTo(map);
   });
 };
